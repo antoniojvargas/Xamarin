2839a7ce07e2553df7f28bcd1d833582562c5e40
anton <anton@DESKTOP-IS3OFQL>
2017-01-25 09:18:40 -0400

    Interface  development

---
+++ AppPortable/AppPortable/AppPortable/MyContentPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppPortable
+{
+    public class MyContentPage : ContentPage
+    {
+        public MyContentPage()
+        {
+            var label = new Label
+            {
+                Text = "Escribe tu nombre"
+            };
+            var txtNombre = new Entry
+            {
+                Placeholder = "Escribe tu nombre"
+            };
+            Content = new StackLayout
+            {
+                Padding = 30,
+                Spacing = 10,
+                Children = { label, txtNombre}
+            };
+        }
+    }
+}
