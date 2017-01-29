f040452ed299e50135e18ab0b129f5fef2dbbbdb
anton <anton@DESKTOP-IS3OFQL>
2017-01-29 16:47:05 -0400

    Scroll Test get a Spiner control

---
+++ ScrollTest/ScrollTest/ScrollTest/App.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ScrollTest
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            //var content = new ContentPage
+            //{
+            //    Title = "ScrollTest",
+            //    Content = new StackLayout
+            //    {
+            //        VerticalOptions = LayoutOptions.Start,
+            //        Padding = 30,
+            //        Spacing = 10,
+            //        Children = {
+            //            new Label {
+            //                HorizontalTextAlignment = TextAlignment.Center,
+            //                Text = "Escribe tu nombre"
+            //            },
+            //            new Entry {
+            //                Placeholder = "Escribe tu nombre"
+            //            },
+            //            new Button {
+            //                Text = "Click me!!!"
+            //            }
+            //        }
+            //    }
+            //};
+
+            MainPage = new MyContentPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ScrollTest/ScrollTest/ScrollTest/MyContentPage.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ScrollTest
+{
+    public class MyContentPage : ContentPage
+    {
+        public MyContentPage()
+        {        
+
+            Picker picker = new Picker
+            {
+                Title = "Seleccione Cereal",
+                VerticalOptions = LayoutOptions.StartAndExpand
+            };
+            picker.Items.Add("Girasol");
+            picker.Items.Add("Soja");            
+            picker.Items.Add("Trigo");
+            picker.Items.Add("Maíz");
+            picker.Items.Add("Sorgo");
+            picker.Items.Add("Cebada");            
+
+            Content = new ScrollView()
+            {
+                Content = new StackLayout()
+                {
+                    Children = {                        
+                        picker,
+                        new Entry(){
+                           Placeholder = "First Name"
+                        },
+                        new Entry(){
+                           Placeholder = "Middle Name"
+                        },
+                        new Entry(){
+                           Placeholder = "Last Name"
+                        },
+                        new Entry(){
+                           Placeholder = "Address Line 1"
+                        },
+                        new Entry(){
+                           Placeholder = "Address Line 2"
+                        },
+                        new Entry(){
+                           Placeholder = "City"
+                        },
+                        new Entry(){
+                           Placeholder = "State Code (Example : OD)"
+                        },
+                        new Entry(){
+                           Placeholder = "Country Code (Example : IN)"
+                        },
+                        new Entry(){
+                           Placeholder = "Zip code"
+                        },
+                        new Entry(){
+                           Placeholder = "Phone number"
+                        },
+                        new Entry(){
+                           Placeholder = "Consumer Number"
+                        },
+                        new Entry(){
+                           Placeholder = "Agent Name"
+                        },
+                        new Entry(){
+                           Placeholder = "Agent Code"
+                        },
+                        new Entry()
+                        {
+                            Placeholder = "Agent Address  Line 1"
+                        },
+                        new Entry()
+                        {
+                            Placeholder = "Agent Address  Line 2"
+                        }
+                     }
+                }
+            };
+        }
+    }
+}
